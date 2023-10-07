960d98a6443a209d81ec07b81b97d19d4e6b86c1
unknown <gausulislam5@gmail.com>
2023-10-08 02:06:36 +0600

    update bar chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import DashBoard from './components/DashBoard/DashBoard'
 import Navbar from './components/Navbar/Navbar'
+import PhoneBar from './components/PhoneBar/PhoneBar'
 import PriceList from './components/PriceList/PriceList'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <h1 className='text-7xl text-purple-600'>Hello From Tailwind</h1>
       <PriceList></PriceList>
       <DashBoard></DashBoard>
+      <PhoneBar></PhoneBar>
     </div>
   )
 }
